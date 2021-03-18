c72bcbbcc3c118c5a3fe1f4fda4820a8005e929d
farvher <farvher@gmail.com>
2021-03-18 16:04:22 -0500

    card modified

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,5 @@
+import { black } from 'ansi-colors';
+import { BackgroundColor } from 'chalk';
 import React from 'react';
 
 const Card = (props) => {
@@ -5,8 +7,8 @@ const Card = (props) => {
     const profile = props;
     if (profile) {
         return (
-            <div className="card w-50">
-                <img src={profile.avatar_url} className="card-img-top" alt="placeholder" />
+            <div className="card">
+                <img src={profile.avatar_url} className="card-img-top img-fluid" alt="placeholder" />
                 <div className="card-body">
                     <div className="card-title">{profile.name}</div>
                     <div className="card-text">{profile.company}</div>
